0fe084a50a4ed8cd63d8d850b06edd7211fd76c7
pholmq <pholmq@gmail.com>
2016-12-02 16:13:55 +0100

    ...

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,19 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
-
 import { Tasks } from '../api/tasks.js';
-
 import Task from './Task.jsx';
-
 //App component repr. the whole App
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
   handleSubmit(event) {
     event.preventDefault();
- 
     // Find the text field via the React ref
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
  
@@ -18,23 +21,40 @@ class App extends Component {
       text,
       createdAt: new Date(), // current time
     });
- 
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
-
   render() {
     return (
       <div className="container">
         <header>
-          <h1> Todo List </h1>
-          
+          <h1> TDS </h1>
+          <label className="hide-completed">
+          <input
+          type="checkbox"
+          readOnly
+          checked={this.state.hideCompleted}
+          onClick={this.toggleHideCompleted.bind(this)}
+          />
+          Hide Completed Tasks
+          </label>
+
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
           <input
             type="text"
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,40 @@
 import React, { Component, PropTypes} from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 //Task component - a single todo item
 export default class Task extends Component {
+  toggleChecked() {
+    //Set checked prop to opposite of curr value
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    //Give tasks a diff...
+
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+    
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
@@ -13,4 +43,4 @@ Task.propTypes = {
   //This comonent get the task to display through a React prop.
   // We can use propTypes to indicate it is required
   task: PropTypes.object.isRequired,
-};
+}
